2f75b715b2de23a62fe3ffac91debfafdb21a9f4
resticDOG <1096340650@qq.com>
2019-03-27 17:33:05 +0800

    first page

--- src/script/controller/companyCtrl.js
+++ src/script/controller/companyCtrl.js
@@ -4,6 +4,16 @@
  * @Date: 2019-03-25 14:29:51
  */
 'use strict';
-angular.module('app').controller('companyCtrl', ['$scope', function($scope){
-    
+angular.module('app').controller('companyCtrl', ['$scope', '$state', '$http', '$q', 
+    function ($scope, $state, $http, $q){
+        function getCom(){
+            var def = $q.defer();
+            $http.get('/data/company.json?id=' + $state.params.id).success(function (res) {
+                def.resolve(res);
+            });
+            return def.promise;
+        }
+        getCom().then(function(res){
+            $scope.company = res;
+        });
 }]);
--- src/script/directive/positionClass.js
+++ src/script/directive/positionClass.js
@@ -8,6 +8,24 @@ angular.module('app').directive('appPositionClass', [function(){
     return {
         restrict : 'A',
         replace : true,
-        templateUrl : 'view/template/positionClass.html'
+        templateUrl : 'view/template/positionClass.html',
+        scope : {
+            com : "="
+        },
+        link : function($scope){
+            $scope.showPositionList = function(idx){
+                // 点击之后需要将当前选中id改变
+                $scope.selectedId = idx;
+                $scope.positionList = $scope.com.positionClass[idx].positionList;
+            };
+            /**
+             *  进入页面时调用一次函数
+             *  这里需要注意，由于请求数据是异步的，在调用此函数的时候com可能无值，
+             *  故需要使用$watch服务监视其状态值的变化
+             **/
+            $scope.$watch('com', function(newVal){
+                if (newVal) $scope.showPositionList(0);
+            });
+        }
     }
 }]);
